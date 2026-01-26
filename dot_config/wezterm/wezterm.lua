7fb83c27fce80e84a2198af4e698cc30f165266f
Eiji Hachiya <eiji.hachiya@medicalshift.co.jp>
2026-01-27 02:54:48 +0900

    wezTermのウィンドウ周りのショートカットをiterm2に合わせた

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -20,6 +21,37 @@ config.default_cursor_style = 'SteadyBlock'
 config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
 config.window_close_confirmation = 'NeverPrompt'
 
+config.keys = {
+  -- Split
+  { key = "d", mods = "CMD", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "D", mods = "CMD", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+
+  -- Pane focus
+  { key = "LeftArrow",  mods = "CMD|ALT", action = act.ActivatePaneDirection "Left" },
+  { key = "RightArrow", mods = "CMD|ALT", action = act.ActivatePaneDirection "Right" },
+  { key = "UpArrow",    mods = "CMD|ALT", action = act.ActivatePaneDirection "Up" },
+  { key = "DownArrow",  mods = "CMD|ALT", action = act.ActivatePaneDirection "Down" },
+
+  -- Resize pane
+  { key = "LeftArrow",  mods = "CMD|ALT|SHIFT", action = act.AdjustPaneSize { "Left", 5 } },
+  { key = "RightArrow", mods = "CMD|ALT|SHIFT", action = act.AdjustPaneSize { "Right", 5 } },
+  { key = "UpArrow",    mods = "CMD|ALT|SHIFT", action = act.AdjustPaneSize { "Up", 5 } },
+  { key = "DownArrow",  mods = "CMD|ALT|SHIFT", action = act.AdjustPaneSize { "Down", 5 } },
+
+  -- Close
+  { key = "w", mods = "CMD", action = act.CloseCurrentPane { confirm = true } },
+  { key = "w", mods = "CMD|SHIFT", action = act.CloseCurrentTab { confirm = true } },
+
+  -- Tabs
+  { key = "t", mods = "CMD", action = act.SpawnTab "CurrentPaneDomain" },
+  { key = "]", mods = "CMD|SHIFT", action = act.ActivateTabRelative(1) },
+  { key = "[", mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1) },
+  { key = "}", mods = "CMD", action = act.MoveTabRelative(1) },
+  { key = "{", mods = "CMD", action = act.MoveTabRelative(-1) },
+
+  -- Zoom
+  { key = "Enter", mods = "CMD|SHIFT", action = act.TogglePaneZoomState },
+}
 
 -- change active tab bar
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
